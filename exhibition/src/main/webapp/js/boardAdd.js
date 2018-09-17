8c651f56264f68fcd29281afecfccf7621ef8c38
kmt1234 <42860611+kmt1234@users.noreply.github.com>
2018-09-17 20:39:26 +0900

    9월17일 마지막

--- exhibition/src/main/webapp/js/boardAdd.js
+++ exhibition/src/main/webapp/js/boardAdd.js
@@ -34,231 +34,64 @@ $(document).ready(function(){
 	
 	//목록 버튼
 	$('.imageboardList').click(function(){
-		if(index=='0'){
+		if($('#postSelect').val('0')){
 			location.href='/exhibition/customerService/C_mainImageboardListForm.do';
-		}else if(index=='1'){
+		}else if($('#postSelect').val('1')){
 			location.href='/exhibition/customerService/C_eventboardListForm.do';
-		}else if(index=='2'){
+		}else if($('#postSelect').val('2')){
 			location.href='/exhibition/customerService/C_eventboardList_playForm.do';
-		}else if(index=='3'){
+		}else if($('#postSelect').val('3')){
 			location.href='/exhibition/customerService/C_hotelListForm.do';	
 		}	
 	});
 	
-	//분류 종류(기본 값 : 0(메인)
-	var index = $('#postSelect option:selected').val();
-	
-	//메인,박람회,연극 선택
-	$('.exhibitionS-td').hide();
-	$('.playS-td').hide();
-	
-	$('.exhibitionL-td').hide();
-	$('.playL-td').hide();
-	$('.hotelL-td').hide();
-	
-	$('.playT-td').hide();
-	$('.exhibitionT-td').hide();
-	$('.eventTime').hide();
-	
-	$('.playP-td').hide();
-	$('.exhibitionP-td').hide();
-	$('.eventPlace').hide();
-	$('.hotelP-td').hide();
-	
-	$('.playPr-td').hide();
-	$('.exhibitionPr-td').hide();
-	$('.eventPrice').hide();
-	
-	$('.playSe-td').hide();
-	$('.eventSeats').hide();
-	
-	$('.playR-td').hide();
-	$('.eventRate').hide();
-	
-	$('.exhibitionI-td').hide();
-	$('.playI-td').hide();
-	
-	$('#postSelect').change(function(){
-		//분류 값 (0:메인 1:박람회 2:연극 3:숙박)
-		
-		index = $('#postSelect option:selected').val();
-		
-		//초기화
-		$('#imageName').val('');
-		$('#img').val('');
-		$('.datepicker2').datepicker().val('');
-		$('#eventLink').val('');
-		$('#eventContent').val('');
-		$('#img2').val('');
 
-		//유효성 안내글
-		$('#imageNameDiv').text('');
-		$('#imgDiv').text('');
-		$('#hotelDiv').text('');
-		$('#telDiv').text('');
-		
-		if(index=='0'){//메인
-			$('#S_td').show();
-			$('.eventS-td').show();
-			$('.exhibitionS-td').hide();
-			$('.playS-td').hide();
-			
-			$('.eventL-td').show();
-			$('.exhibitionL-td').hide();
-			$('.playL-td').hide();
-			$('.hotelL-td').hide();
-			
-			$('.I_tr').show();
-			$('.eventI-td').show();
-			$('.exhibitionI-td').hide();
-			$('.playI-td').hide();
-			
-			
-			$('.playT-td').hide();
-			$('.exhibitionT-td').hide();
-			$('.eventTime').hide();
-			
-			$('.playP-td').hide();
-			$('.exhibitionP-td').hide();
-			$('.eventPlace').hide();
-			$('.hotelP-td').hide();
-			
-			$('.playPr-td').hide();
-			$('.exhibitionPr-td').hide();
-			$('.eventPrice').hide();
-			
-			$('.playSe-td').hide();
-			$('.playR-td').hide();
-			
-			$('.eventSeats').hide();
-			$('.eventRate').hide();
-			
-						
-		}else if(index=='1'){//박람회
-			$('#S_td').show();
-			$('.eventS-td').hide();
-			$('.exhibitionS-td').show();
-			$('.playS-td').hide();
-			
-			$('.eventL-td').hide();
-			$('.exhibitionL-td').show();
-			$('.playL-td').hide();
-			$('.hotelL-td').hide();
-			
-			$('.I_tr').show();
-			$('.eventI-td').hide();
-			$('.exhibitionI-td').show();
-			$('.playI-td').hide();
-			
-			
-			$('.playT-td').hide();
-			$('.exhibitionT-td').show();
-			$('.eventTime').show();
-			
-			$('.playP-td').hide();
-			$('.exhibitionP-td').show();
-			$('.hotelP-td').hide();
-			$('.eventPlace').show();
-			
-			$('.playPr-td').hide();
-			$('.exhibitionPr-td').show();
-			$('.eventPrice').show();
-			
-			$('.playSe-td').hide();
-			$('.eventSeats').hide();
-				
-			$('.playR-td').hide();
-			$('.eventRate').hide();
-							
-		}else if(index=='2'){//연극
-			$('#S_td').show();
-			$('.eventS-td').hide();
-			$('.exhibitionS-td').hide();
-			$('.playS-td').show();
-			
-			$('.eventL-td').hide();
-			$('.exhibitionL-td').hide();
-			$('.playL-td').show();
-			$('.hotelL-td').hide();
-			
-			$('.I_tr').show();
-			$('.eventI-td').hide();
-			$('.exhibitionI-td').hide();
-			$('.playI-td').show();
-			
-			
-			$('.playT-td').show();
-			$('.exhibitionT-td').hide();
-			$('.eventTime').show();
-			
-			$('.playP-td').show();
-			$('.exhibitionP-td').hide();
-			$('.eventPlace').show();
-			$('.hotelP-td').hide();
-			
-			$('.playPr-td').show();
-			$('.exhibitionPr-td').hide();
-			$('.eventPrice').show();
-			
-			$('.playSe-td').show();
-			$('.eventSeats').show();
-			
-			$('.playR-td').show();
-			$('.eventRate').show();
-			
-		}else if(index=='3'){//숙박
-			$('#S_td').hide();
-			$('.eventS-td').hide();
-			$('.exhibitionS-td').hide();
-			$('.playS-td').hide();
-			
-			$('.eventL-td').hide();
-			$('.exhibitionL-td').hide();
-			$('.playL-td').hide();
-			$('.hotelL-td').show();
-			$('#eventLink').val('http://');
-
-			$('.I_tr').hide();
-			
-			$('.playT-td').hide();
-			$('.exhibitionT-td').hide();
-			$('.eventTime').hide();
-			
-			$('.playP-td').hide();
-			$('.exhibitionP-td').hide();
-			$('.hotelP-td').show();
-			$('.eventPlace').show();
-			
-			$('.playPr-td').hide();
-			$('.exhibitionPr-td').hide();
-			$('.eventPrice').hide();
-			
-			$('.playSe-td').hide();
-			$('.playR-td').hide();
-			
-			$('.eventSeats').hide();
-			$('.eventRate').hide();
-		}
-	});
-		
 	
-	//이미지 등록
-	$('#checkImageboardWrite').click(function(){
+	$('#imageTap').click(function(){
+		$('#postSelect').val('0');
 		$('#imageNameDiv').empty();
 		$('#imgDiv').empty();
 		$('#hotelDiv').empty();
 		$('#telDiv').empty();
-		
-		if(index=='0' ){
+		alert($('#postSelect').val());
+	});
+	$('#exhibitionTap').click(function(){
+		$('#postSelect').val('1');
+		$('#imageNameDiv').empty();
+		$('#imgDiv').empty();
+		$('#hotelDiv').empty();
+		$('#telDiv').empty();
+		alert($('#postSelect').val());
+	});
+	$('#PlayTap').click(function(){
+		$('#postSelect').val('2');
+		$('#imageNameDiv').empty();
+		$('#imgDiv').empty();
+		$('#hotelDiv').empty();
+		$('#telDiv').empty();
+		alert($('#postSelect').val());
+	});
+	$('#HotelTap').click(function(){
+		$('#postSelect').val('3');
+		$('#imageNameDiv').empty();
+		$('#imgDiv').empty();
+		$('#hotelDiv').empty();
+		$('#telDiv').empty();
+		alert($('#postSelect').val());
+	});
+	
+	//이미지 등록
+	$('#checkImageboardWrite').click(function(){
+		if($('#postSelect').val('0')){
 			if($('#imageName').val()=='')
 				$('#imageNameDiv').text('제목을 입력하세요').css('color','red').css('font-size','9pt').css('font-weight','bold');
 			else if($('#img').val()=='') 
 				$('#imgDiv').text('파일을 선택해 주세요').css('color','magenta').css('font-size','9pt').css('font-weight','bold');
 			else//메인 이미지 슬라이더
-				$('#imageboardWriteForm').attr({action:'/exhibition/customerService/C_imageboardWrite.do', method:'post'}).submit();
+				$('#imageboardWriteForm2').attr({action:'/exhibition/customerService/C_imageboardWrite.do', method:'post'}).submit();
 		}
 		
-		else if(index=='1' || index=='2'){
+		else if($('#postSelect').val('1') || $('#postSelect').val('2')){
 			alert('시간1 : '+$('.timepicker1').val());
 			alert('시간2 : '+$('.timepicker2').val());
 			
@@ -277,12 +110,12 @@ $(document).ready(function(){
 			/*else if($('.timepicker1').val() < $('.timepicker2').val()){
 				alert('시간2가 시간1 보다 작음');
 			}*/
-			else if(index==1){	//박람회
-				$('#imageboardWriteForm').attr({action:'/exhibition/customerService/C_eventInfoWrite.do', method:'post'}).submit();
-			}else if(index==2){	//연극
-				$('#imageboardWriteForm').attr({action:'/exhibition/customerService/C_eventInfoWrite_play.do', method:'post'}).submit();
+			else if($('#postSelect').val('1')){	//박람회
+				$('#eventboardWriteForm2').attr({action:'/exhibition/customerService/C_eventInfoWrite.do', method:'post'}).submit();
+			}else if($('#postSelect').val('2')){	//연극
+				$('#playboardWriteForm2').attr({action:'/exhibition/customerService/C_eventInfoWrite_play.do', method:'post'}).submit();
 			}
-		}else if(index=='3'){
+		}else if($('#postSelect').val('3')){
 			var reg = /^https?\:\/\/.+/;
 			var tel = /^((01[1|6|7|8|9])[1-9]+[0-9]{6,7})|(010[1-9][0-9]{7})|(02|0[3-9]+[0-9])(\d{3,4})(\d{4})$/;
 			if($('#imageName').val()=='')
@@ -298,7 +131,7 @@ $(document).ready(function(){
 			else if(!tel.test($('#telPlace').val())){
 				$('#telDiv').text('전화번호 양식에 맞지 않습니다').css('color','red').css('font-size','9pt').css('font-weight','bold');
 			}else
-				$('#imageboardWriteForm').attr({action:'/exhibition/customerService/C_hotelInfoWrite.do', method:'post'}).submit();
+				$('#hotelboardWriteForm2').attr({action:'/exhibition/customerService/C_hotelInfoWrite.do', method:'post'}).submit();
 		}
 		
 	});
