126fd6f5960686568ecf526c30558709fa62ad8e
shooter-jbi <jbi8045@gmail.com>
2018-09-20 17:28:58 +0900

    "콘서트 홀 매출 중간 커밋"

--- exhibition/src/main/java/rental/dao/ExhibitionDAO.java
+++ exhibition/src/main/java/rental/dao/ExhibitionDAO.java
@@ -3,7 +3,7 @@ package rental.dao;
 import java.util.List;
 import java.util.Map;
 
-import customerService.bean.SalesExhigitionDTO;
+import customerService.bean.SalesExhibitionDTO;
 import performance.bean.PerformanceDTO;
 import rental.bean.ConcertHallDTO;
 import rental.bean.ExhibitionDTO;
